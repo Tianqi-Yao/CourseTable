0b454196665837b9221c72395ef328cfde79bfe4
Tianqi <yaotianqi6666@qq.com>
2022-10-05 13:11:51 -0500

    first time update

--- app/src/main/java/com/example/coursetable/MainActivity.kt
+++ app/src/main/java/com/example/coursetable/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.example.coursetable
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // test github
+
+        val bt = findViewById<Button>(R.id.btn_login)
+
+
+        bt.setOnClickListener {
+//            Log.d("test", "onclick btn")
+//            println("this is print test")
+//            Toast.makeText(this, "this is toast test", Toast.LENGTH_LONG).show()
+            jumpFun()
+        }
+    }
+
+    private fun jumpFun() {
+        val intent = Intent(this, MainActivity2::class.java)
+//        val msg = "message"
+//        intent.putExtra("msg",msg)
+//        val et = findViewById<EditText>(R.id.editText)
+//        intent.putExtra("et",et.text.toString())
+        startActivity(intent)
+
     }
 }
